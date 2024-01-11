25c86498269e68a026b04e0d02e2c65cf9e4927b
judejay <judejay26@gmail.com>
2024-01-11 16:45:15 +0000

    Install typescript

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+//const express = require("express");
+
+//const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
